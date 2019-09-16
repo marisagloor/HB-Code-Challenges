93809e8a2a50d6a28b565085d7d8fd71039c619e
Marisa Gloor <marisagloor@gmail.com>
2019-09-16 11:54:11 -0700

    Challenge 1: Complete - CircArray class with rotation and indexing

--- circular-array/circular_array.py
+++ circular-array/circular_array.py
@@ -89,12 +89,22 @@ class CircularArray(object):
 
     def __init__(self):
         """Instantiate CircularArray."""
+        
+        self.items = []
+
 
     def add_item(self, item):
         """Add item to array, at the end of the current rotation."""
+        
+        self.items.append(item)
+
 
     def get_by_index(self, index):
         """Return the data at a particular index."""
+        
+        if index <= len(self.items) - 1:
+            return self.items[index]
+
 
     def rotate(self, increment):
         """Rotate array, positive for right, negative for left.
@@ -102,8 +112,21 @@ class CircularArray(object):
         If increment is greater than list length, keep going around.
         """
 
+        increment = increment % len(self.items)
+        new_end = self.items[:increment]
+        new_beginning = self.items[increment:]
+        self.items = new_beginning + new_end
+
+
     def print_array(self):
         """Print the circular array items in order, one per line"""
+        
+        for item in self.items:
+            print(item)
+
+
+
+
 
 if __name__ == "__main__":
     print()
