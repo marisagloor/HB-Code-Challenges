87c54577ee03034476ee2f3a5a290e75023ebf8c
Marisa Gloor <marisagloor@gmail.com>
2019-09-17 15:33:18 -0700

    Challenge 3: Complete - finds most common date range

--- most-active/active.py
+++ most-active/active.py
@@ -34,6 +34,32 @@ Since there's a tie, the first was returned)
 
 def most_active(bio_data):
     """Find window of time when most authors were active."""
+    inds_to_check = [0]
+    checked = {0}
+    poss = {}
+    while inds_to_check:
+        count = 0
+        ind = inds_to_check.pop()
+        low = bio_data[ind][1]
+        high = bio_data[ind][2]
+        for i, auth in enumerate(bio_data):
+            if (auth[1] <= low & auth[2] > low) | (auth[2] >= high & auth[1] < high) | (auth[1] > low & auth[2] < high):
+                count += 1
+            if high > auth[1] > low:
+                low = auth[1]
+            if high > auth[2] > low:
+                high = auth[2]
+            if (auth[2] < low < high | auth[1] > high > low) & (not i in checked):
+                inds_to_check.append(i)
+                checked.append(i)
+        if count in poss.keys():
+            if low < poss[count][0]:
+                poss[count] = (low, high)
+        else:
+            poss[count] = (low, high)
+
+        return poss[max(poss.keys())]
+        
 
 if __name__ == '__main__':
     import doctest
