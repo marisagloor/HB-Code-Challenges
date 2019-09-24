4bd4d8e154cb585f9306105d5d6ccdd4961808c8
Marisa Gloor <marisagloor@gmail.com>
2019-09-24 15:22:14 -0700

    Challenge 4: Complete

--- lazy-lemmings/lemmings.py
+++ lazy-lemmings/lemmings.py
@@ -36,6 +36,52 @@ Find the farthest any single lemming needs to travel for food.
 
 def furthest(num_holes, cafes):
     """Find longest distance between a hole and a cafe."""
+    largest = 0
+    if cafes[0] > largest:
+        largest = cafes[0]
+
+    if num_holes - 1 - cafes[-1] > largest:
+        largest = num_holes - 1 - cafes[-1]
+
+    if len(cafes) > 2:
+        for i, cafe in enumerate(cafes[1:-1]):
+            lower = int((cafe - cafes[i])/ 2)
+            higher = int((cafes[i + 2] - cafe) / 2)
+            if lower > largest:
+                largest = lower
+            if higher > largest:
+                largest = higher
+    else:
+        mid = int((cafes[-1] - cafes[0])/2)
+        if mid > largest:
+            largest = mid
+        
+    return largest
+
+
+def furthest_optimized(num_holes, cafes):
+    largest = 0
+    if cafes[0] > largest:
+        largest = cafes[0]
+
+    if num_holes - 1 - cafes[-1] > largest:
+        largest = num_holes - 1 - cafes[-1]
+
+    if len(cafes) > 2:
+        for i, cafe in enumerate(cafes[1:-1]):
+            lower = int((cafe - cafes[i])/ 2) 
+            higher = int((cafes[i + 2] - cafe) / 2)
+            if lower > largest:
+                largest = lower
+            if higher > largest:
+                largest = higher
+    else:
+        mid = int((cafes[-1] - cafes[0])/2)
+        if mid > largest:
+            largest = mid
+
+    return largest
+
 
 if __name__ == '__main__':
     import doctest
